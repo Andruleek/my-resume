aa06e52ec8b8fc671c297a147fc2a56756e8ca20
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-02-10 23:20:52 +0200

    ура commit

--- ran35.py
+++ ran35.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
-from shutil import move
-from zipfile import ZipFile, BadZipFile
+import patoolib
 import sys
 
 
@@ -36,7 +35,7 @@ def sort_files(folder_path):
 
             destination_path = folder_path / destination_folder / normalized_filename
             try:
-                move(file_path, destination_path)
+                os.rename(file_path, destination_path)
                 print(f"Moved {filename} to {destination_path}")
             except FileNotFoundError:
                 print(f"Failed to move {filename}. File not found.")
@@ -47,23 +46,21 @@ def sort_files(folder_path):
         remove_empty_directories(folder_path)
 
 
-def extract_archives(archive_folder):
+def extract_archives(folder_path):
     print("Extracting archives...")
-    for root, dirs, files in os.walk(archive_folder):
+    for root, dirs, files in os.walk(folder_path):
         for archive_filename in files:
             archive_path = Path(root) / archive_filename
             _, archive_extension = os.path.splitext(archive_filename)
             normalized_filename = normalize(archive_filename)
 
             if archive_extension.lower() in ['.zip', '.gz', '.tar']:
-                extract_folder = archive_folder / 'archives' / normalized_filename.removesuffix(archive_extension)
-                with ZipFile(archive_path, 'r') as zip_ref:
-                    try:
-                        zip_ref.extractall(extract_folder)
-                        print(f"Extracted {archive_filename} to {extract_folder}")
-                    except BadZipFile:
-                        print(f"Failed to extract {archive_filename} in {archive_folder}. Removing...")
-                        archive_path.unlink()
+                extract_folder = folder_path / 'archives' / normalized_filename.removesuffix(archive_extension)
+                try:
+                    patoolib.extract_archive(archive_path, outdir=extract_folder)
+                    print(f"Extracted {archive_filename} to {extract_folder}")
+                except Exception as e:
+                    print(f"Failed to extract {archive_filename} in {folder_path}. Error: {e}")
 
         # Remove empty directories
         remove_empty_directories(folder_path)
---
+++ readme.txt
@@ -0,0 +1,13 @@
+C:/Users/Admin/AppData/Lokal/Programs/Python310/python.exe D:/sort/ran.py D:/sort/test_garbage_folder_no_nosuffix
+
+
+C:/Users/Admin/Desktop/test_garbage_folder_no_nosuffix
+
+D:/sort/test_garbage_folder_no_nosuffix
+
+
+python ran.py D:/pylint/test_garbage_folder_no_nosuffix
+
+ c:/Users/Admin/Desktop/pylint/ran.py
+
+python ran35.py C:\Users\Admin\Desktop\my-resume\test_garbage_folder_no_nosuffix
